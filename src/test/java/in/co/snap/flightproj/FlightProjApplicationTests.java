98973e532fe448dd6845353950b061df3677793d
AmarJeetSahi <amarjeet.sahi70@gmail.com>
2023-09-22 22:09:36 +0530

    init

---
+++ src/test/java/in/co/snap/flightproj/FlightProjApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.snap.flightproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
